47cc7620ab9007cf0b45e59fb40af1cd49ec4c9d
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-04-25 20:54:38 +0800

    menambahkan library axios

--- src/Praktikum/Week9.js
+++ src/Praktikum/Week9.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import "../App"
 import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Week9 extends React.Component {
 
@@ -24,15 +26,24 @@ class Week9 extends React.Component {
   render(){
 		const { users } = this.state
     return(
-      <div>
-				{ users.length < 1 && <div>loading...</div>}
-
+  	  <div>
         { this.state.users.map( user=> {
 					return (
-						<div>{ user.name }</div>
+						<div className="container">
+							<div class="card border-info mb-3">
+								<div class="card-header">Biodata Pengguna</div>
+								<div class="card-body text-secondary">
+									<h5 class="card-title">{ user.name }</h5>
+									<p class="card-text">Username : { user.username }</p>
+									<p class="card-text">E-mail : { user.email }</p>
+									<p class="card-text">Phone Number : { user.phone }</p>
+									<p class="card-text">Company : { user.company.name }</p>
+								</div>
+							</div>
+						</div>
 					)
 				})}
-      </div>
+    	</div>
     )
   }
 }
